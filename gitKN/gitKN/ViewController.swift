b9ed4c28a763bfedbf1d4c6f85ab6bc4de869fcc
wenmohen <2090963192@qq.com>
2017-09-20 21:57:05 +0800

    修改

--- gitKN/gitKN/ViewController.swift
+++ gitKN/gitKN/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 print("你好")
+        print("werwe")
+
     
     }
 
