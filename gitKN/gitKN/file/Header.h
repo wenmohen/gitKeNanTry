b9c6846560d668d132ca7604c357cadfebbd21dc
wenmohen <2090963192@qq.com>
2017-09-20 21:42:21 +0800

    添加宏文件

---
+++ gitKN/gitKN/file/Header.h
@@ -0,0 +1,13 @@
+//
+//  Header.h
+//  gitKN
+//
+//  Created by nhl on 2017/9/20.
+//  Copyright © 2017年 nhl. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+#define width 20
+#endif /* Header_h */
